a83924c41742305bd1478908f1fbd2b54b5b43f4
Lucas Ketels <lucas.ketels@gmail.com>
2021-05-15 16:25:20 +0200

    Add 2 requests

--- allRequests.sql
+++ allRequests.sql
@@ -46,14 +46,17 @@ AND PR2.id_produit = V2.id_produit
 AND V1.nouveauPrix = V2.nouveauPrix;
 
 -- V)
--- Contrainte : Calcul de deux agrégats
---
--- Remarque :
+-- Contrainte : Calcul de deux agrégats + Sous-requête dans le FROM.
+-- La sous-reqête de FROM calcul le prix maximum d'un article d'un certain type vendu par une certaine marque. La requête principale
+-- calcule la moyenne de ces prix maximums par marques.
+-- Remarque : Le résultat de la requête n'est peut-être pas très intéressant
 SELECT REQ.marque, AVG(REQ.prix_max)
-FROM (SELECT PR.type_produit, PR.marque, MAX(PR.prix) as prix_max
-FROM produits PR
-WHERE PR.marque IS NOT NULL
-GROUP BY PR.type_produit, PR.marque) AS REQ
+FROM (
+    SELECT PR.type_produit, PR.marque, MAX(PR.prix) as prix_max
+    FROM produits PR
+    WHERE PR.marque IS NOT NULL
+    GROUP BY PR.type_produit, PR.marque
+    ) AS REQ
 GROUP BY REQ.marque;
 
 -- VI)
@@ -69,7 +72,7 @@ GROUP BY PR.marque;
 
 
 -- VII)
---
+-- Contrainte :
 -- Calcul les dépensens moyennes de chaque client
 -- Remarque : 
 SELECT C.nom, C.prenom, AVG(CO.quantite * PR.prix)
@@ -77,3 +80,71 @@ FROM comptes C, produits PR, commandes CO
 WHERE C.ID_compte = CO.ID_compte
 AND PR.id_produit = CO.id_produit
 GROUP BY C.nom, C.prenom;
+
+-- VIII)
+-- Contrainte : Sous-requête dans le WHERE
+-- Trouve toutes les marques qui ne vendent pas des produits de type 'Epicerie'.
+-- Remarque :
+SELECT DISTINCT PR1.marque 
+FROM produits PR1
+WHERE PR1.marque NOT IN
+(SELECT DISTINCT PR2.marque 
+FROM produits PR2
+WHERE PR2.marque IS NOT NULL
+AND PR2.type_produit = 'Epicerie');
+
+-- IX)
+-- Contrainte : Condition de totalité + sous-requêtes corrélées (+ équivalente à X))
+-- Trouver les marques dont tous les produits ont toutes leurs notes au dessus de 3 (si la note n'est pas null)
+-- Remarque :
+
+-- X)
+-- Contrainte : Condition de totalité + agrégation (+ équivalente à IX))
+-- Trouver les marques dont tous les produits commandés ont tous leurs notes au dessus de 3 (si la note n'est pas null)
+-- Remarque :
+SELECT REQ.marque FROM (
+    SELECT PR.marque, CO.note
+    FROM produits PR, commandes CO
+    WHERE PR.id_produit = CO.id_produit
+    AND CO.note IS NOT NULL
+    AND PR.marque IS NOT NULL
+    AND CO.note > 3
+    ) AS REQ
+GROUP BY REQ.marque
+HAVING COUNT(REQ.note) = (
+    SELECT COUNT(CO2.note)
+    FROM commandes CO2, produits PR2
+    WHERE CO2.id_produit = PR2.id_produit
+    AND CO2.note IS NOT NULL
+    AND PR2.marque = REQ.marque
+    );
+
+-- XI)
+-- Contrainte : Jointure externe
+--
+-- Remarque :
+SELECT PR.id_produit, PR.nom, CO.id_commande
+FROM commandes CO
+LEFT OUTER JOIN produits PR
+ON PR.id_produit = CO.id_produit;
+
+-- XII)
+-- Contrainte : requêtes équivalentes sans null, mais différentes avec
+--
+-- Remarque :
+
+-- XIII)
+-- Contrainte : requêtes équivalentes sans null, mais différentes avec
+--
+-- Remarque :
+
+-- XIV)
+-- Contrainte :
+--
+-- Remarque :
+
+-- XV)
+-- Contrainte :
+--
+-- Remarque :
+
